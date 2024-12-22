5879e4736b1732fac6c98bbdb506748a7001d9f2
Weeeggs <wiga.wna29@gmail.com>
2024-12-22 13:17:50 +0700

    ui

--- Script/main_menu.gd
+++ Script/main_menu.gd
@@ -7,3 +7,7 @@ func _on_mainkan_pressed() -> void:
 
 func _on_keluar_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_tentang_game_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/tentang.tscn")
